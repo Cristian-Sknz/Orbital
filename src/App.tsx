ebf90ffbef20f601f52c9f9aa45f371b4ed4c816
dedecanton <andrecanton18@gmail.com>
2021-11-06 15:52:16 -0300

    "Galeria" section completed

--- src/App.tsx
+++ src/App.tsx
@@ -8,15 +8,17 @@ import Inicio from './components/sections/Inicio';
 import Sobre from './components/sections/Sobre/';
 import Servicos from './components/sections/Servicos';
 import Projetos from './components/sections/Projetos';
+import Galeria from './components/sections/Galeria';
 
 const App: React.FC = () => {
   return (
     <>
       <Navbar />
       <Inicio />
-      <Sobre/>
-      <Servicos/>
+      <Sobre />
+      <Servicos />
       <Projetos />
+      <Galeria />
     </>
   );
 };
