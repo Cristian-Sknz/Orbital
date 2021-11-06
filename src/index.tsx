13a39fc79dc90161483fe79f9d3b8cb716b2af81
Cristian "SknZ" Ferreira <cristian.datingaa@gmail.com>
2021-11-06 17:28:52 -0300

    feature: importado os icones que serão utilizados

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/reset.css';
 import App from './App';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import ApplicationIcons from './lib/ApplicationIcons';
+
+ApplicationIcons.forEach((item) => {
+  library.add(item);
+}) ;
 
 ReactDOM.render(
   <React.StrictMode>
---
+++ src/lib/ApplicationIcons.ts
@@ -0,0 +1,40 @@
+import {
+  faAdjust,
+  faAngleRight,
+  faArrowLeft,
+  faArrowRight,
+  faArrowUp,
+  faBars,
+  faCode,
+  faExternalLinkAlt,
+  faPencilRuler,
+  faPoll,
+  faTimes,
+} from '@fortawesome/free-solid-svg-icons';
+
+import {
+  faGithub,
+  faInstagram,
+  faLinkedin,
+  faWhatsapp,
+} from '@fortawesome/free-brands-svg-icons';
+
+const ApplicationIcons = [
+  faAdjust,
+  faAngleRight,
+  faArrowLeft,
+  faArrowRight,
+  faArrowUp,
+  faBars,
+  faCode,
+  faExternalLinkAlt,
+  faPencilRuler,
+  faPoll,
+  faTimes,
+  faGithub,
+  faInstagram,
+  faLinkedin,
+  faWhatsapp,
+];
+
+export default ApplicationIcons;
