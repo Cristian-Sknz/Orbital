1b0eb5ad13c2778fcac5cbe9558935c8b0ea8adb
dedecanton <andrecanton18@gmail.com>
2021-11-06 11:06:42 -0300

    "sobre" section completed

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,16 @@ import React from 'react';
 import './index.css';
 import './theme/App.css';
 
-import Inicio from './components/sections/Inicio';
 import Navbar from './components/models/Navbar';
+import Inicio from './components/sections/Inicio';
+import Sobre from './components/sections/Sobre/';
 
 const App: React.FC = () => {
   return (
     <>
       <Navbar />
       <Inicio />
+      <Sobre/>
     </>
   );
 };
