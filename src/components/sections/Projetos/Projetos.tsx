77819df9aad6a82261608f2fd9ed2421563a323d
Cristian "SknZ" Ferreira <cristian.datingaa@gmail.com>
2021-11-18 17:19:50 -0300

    feature: atualizado projetos e imagens

--- src/components/sections/Projetos/Projetos.tsx
+++ src/components/sections/Projetos/Projetos.tsx
@@ -3,17 +3,16 @@ import './projetos.css';
 import Projeto from './Projeto';
 import OrbitalSection from '../OrbitalSection';
 
-import * as json from '~/assets/data/projetos.json';
+import Projetcs from '~/assets/data/projetos.json';
 import useReveal from '~/lib/hooks/useReveal';
 
 const Projetos = () => {
   const project = useReveal<HTMLUListElement>('top');
-  const projects = json.projects;
 
   return (
     <OrbitalSection id={'projetos'} title={'Projetos'} showTitle={true}>
       <ul className='project-row' ref={project}>
-        {projects.map((project, index) => {
+        {Projetcs.map((project, index) => {
           return (
             <Projeto
               title={project.title}
